3bf07108dae6f0e74aa497969278bfe4655c6f15
Rob Butcher <robbutcher2001@gmail.com>
2021-02-03 17:44:05 +0000

    Button impl

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,44 @@
-import React from "react";
+import React, { FC } from "react";
 
-export const StartButton = () => <button>Start</button>;
-export const StopButton = () => <button>Stop</button>;
+type StylingProps = {
+  className: string;
+};
+
+type ConfigurableButtonProps = {
+  callback: () => void;
+  disabled: boolean;
+  autoFocus?: boolean;
+};
+
+type ButtonProps = StylingProps & ConfigurableButtonProps;
+
+const Button: FC<ButtonProps> = ({
+  className,
+  callback,
+  disabled,
+  autoFocus,
+  children,
+}) => {
+  return (
+    <button
+      type="button"
+      className={className}
+      onClick={callback}
+      disabled={disabled}
+      autoFocus={autoFocus}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const StartButton = (props: ConfigurableButtonProps) => (
+  <Button className="start" {...props}>
+    Start
+  </Button>
+);
+export const StopButton = (props: ConfigurableButtonProps) => (
+  <Button className="stop" {...props}>
+    Stop
+  </Button>
+);
